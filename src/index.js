dfa020ac31cb547d79c35fbd5666ece7ea745120
martahj <marta.hourigan.johnson@gmail.com>
2017-11-28 16:57:08 -0600

    build(src): Export new components
    Exports all the components created since the first version of the
    package was created.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 export { default as Button } from './components/Button';
+export { default as TextField } from './components/TextField';
+export * from './components/Card';
 export { default as withRipple } from './decorators/withRipple';
 export { default as ThemeProvider } from './theme/ThemeProvider';

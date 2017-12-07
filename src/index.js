f7c172f98e0911bbcfb4a77845b2a0ef188b836d
kristajg <krista.goralczyk@conciergeauctions.com>
2017-12-07 13:25:42 -0600

    fix(Tabs): Exports Tabs
    Fixes small bug where Tabs and Tab were
    not being exported in the index.js.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export { default as Button } from './components/Button';
 export { default as TextField } from './components/TextField';
 export * from './components/Card';
+export * from './components/Tabs';
 export { default as withRipple } from './decorators/withRipple';
 export { default as ThemeProvider } from './theme/ThemeProvider';

644baa2a17805122644e3a9c99afce1b12dbf5fe
martahj <marta.hourigan.johnson@gmail.com>
2018-01-08 15:06:54 -0600

    fix(withRipple export): Export from correct place
    Exports withRipple from the mixins folder instead of the nonexistent
    decorators folder.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ export { default as Button } from './components/Button';
 export { default as TextField } from './components/TextField';
 export * from './components/Card';
 export * from './components/Tabs';
-export { default as withRipple } from './decorators/withRipple';
+export { default as withRipple } from './mixins/ripple';
 export { default as ThemeProvider } from './theme/ThemeProvider';
 export { default as defaultTheme } from './theme/defaultTheme';

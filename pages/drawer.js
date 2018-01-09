4e473814a842babc328627af4a7b70a5396f1155
Brad Decker <bhdecker84@gmail.com>
2018-01-09 08:54:04 -0600

    feat(Drawer): Add Drawer component
    Adds the material ui drawer component with most major features. This
    doesn't do any styling on the contents of the drawer -- that will likely
    fall into the hands of the navigation component developer as that is the
    primary true material design use case.

---
+++ pages/drawer.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import MaterialThemeProvider from '../src/theme/ThemeProvider';
+import { Drawer } from '../src/components/Drawer';
+import Button from '../src/components/Button';
+
+const PageContainer = styled.div`
+  padding: 10px;
+`;
+
+export default class PortalPage extends Component {
+  state = {
+    temporaryLeft: false,
+    temporaryRight: false,
+    persistentLeft: false,
+    persistentRight: false,
+  };
+
+  toggleTemporaryLeft = () => this.setState(prevState => ({
+    temporaryLeft: !prevState.temporaryLeft,
+  }));
+
+  toggleTemporaryRight = () => this.setState(prevState => ({
+    temporaryRight: !prevState.temporaryRight,
+  }));
+
+  togglePersistentLeft = () => this.setState(prevState => ({
+    persistentLeft: !prevState.persistentLeft,
+    persistentRight: false,
+  }));
+
+  togglePersistentRight = () => this.setState(prevState => ({
+    persistentRight: !prevState.persistentRight,
+    persistentLeft: false,
+  }));
+
+
+  render() {
+    return (
+      <MaterialThemeProvider theme={{ primary: '#03A9F4' }}>
+        <PageContainer>
+          <h1>Temporary Drawers!</h1>
+          <Drawer
+            temporary
+            attachment="left"
+            open={this.state.temporaryLeft}
+            handleRequestClose={this.toggleTemporaryLeft}>
+            <p>{'Hi, im a temporary left attached drawer'}</p>
+          </Drawer>
+          <Drawer
+            temporary
+            attachment="right"
+            open={this.state.temporaryRight}
+            handleRequestClose={this.toggleTemporaryRight}>
+            <p>{'Hi, im a temporary right attached drawer'}</p>
+          </Drawer>
+          <Button raised primary onClick={this.toggleTemporaryRight}>Right Temporary</Button>
+          <Button raised accent onClick={this.toggleTemporaryLeft}>Left Temporary</Button>
+          <h1>Persistent Drawers!</h1>
+          <Drawer
+            attachment="left"
+            open={this.state.persistentLeft}
+            handleRequestClose={this.togglePersistentLeft}>
+            <p>{'Hi, im a persistent left attached drawer'}</p>
+          </Drawer>
+          <Drawer
+            attachment="right"
+            open={this.state.persistentRight}
+            handleRequestClose={this.togglePersistentRight}>
+            <p>{'Hi, im a persistent right attached drawer'}</p>
+          </Drawer>
+          <Button raised primary onClick={this.togglePersistentRight}>Right Persistent</Button>
+          <Button raised accent onClick={this.togglePersistentLeft}>Left Persistent</Button>
+        </PageContainer>
+      </MaterialThemeProvider>
+    );
+  }
+}
+

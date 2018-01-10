3dfeb8d641b9a75c8b2b65cc39653918c85e4a8e
martahj <marta.hourigan.johnson@gmail.com>
2018-01-10 14:40:45 -0600

    fix(Exports): Export new components
    Exports new components from index.js so they can be imported.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+export { Avatar } from './components/Avatar';
 export { default as Button } from './components/Button';
-export { default as TextField } from './components/TextField';
 export * from './components/Card';
+export * from './components/Dialog';
+export * from './components/List';
+export { Portal } from './components/Portal';
+export * from './components/Slider';
+export * from './components/Table';
 export * from './components/Tabs';
+export { default as TextField } from './components/TextField';
 export { default as withRipple } from './mixins/ripple';
 export { default as ThemeProvider } from './theme/ThemeProvider';
 export { default as defaultTheme } from './theme/defaultTheme';

6d8669c592660a541f92337b63f681033e5359d6
ashwoodall[user] <ash.woodall@gmail.com>
2018-01-22 16:53:02 -0600

    fix(Drawer): Export Drawer component
    Export Drawer component in index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+export { default as Appbar } from './components/Appbar';
 export { Avatar } from './components/Avatar';
 export { default as Button } from './components/Button';
 export * from './components/Card';
 export * from './components/Dialog';
 export { default as Divider } from './components/Divider';
+export { default as Drawer } from './components/Drawer';
 export * from './components/List';
 export { Portal } from './components/Portal';
 export * from './components/Slider';
@@ -10,7 +12,6 @@ export { default as Snackbar } from './components/Snackbar';
 export * from './components/Table';
 export * from './components/Tabs';
 export { default as TextField } from './components/TextField';
-export { default as Appbar } from './components/Appbar';
 
 export { default as withRipple } from './mixins/ripple';
 

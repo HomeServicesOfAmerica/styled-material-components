5f70b7a13fe29b066200077a308a6f2388e385a0
ashwoodall[user] <ash.woodall@gmail.com>
2018-01-25 13:48:36 -0600

    feat(Grid List): Add Grid List SMC
    MVP Grid List implementation
    
    Resolve #24

---
+++ pages/gridlists.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import styled from 'styled-components';
+import MaterialThemeProvider from '../src/theme/ThemeProvider';
+import { GridList, GridTile } from '../src/components/GridList';
+
+const Img = styled.img`
+  object-fit: cover;
+  width: 230px;
+  height: 230px;
+`;
+
+const tilesData = [
+  {
+    img: 'https://media2.popsugar-assets.com/files/2016/02/08/898/n/1922398/4c2124bfd07afeca_heart-shaped-pizza-2016.xxxlarge_2x.jpg',
+  },
+  {
+    img: 'https://www.livekindly.co/wp-content/uploads/2017/08/pizza-vegan-1280x640.jpg',
+  },
+  {
+    img: 'https://www.the-girl-who-ate-everything.com/wp-content/uploads/2014/07/best-baked-beans-6.jpg',
+  },
+  {
+    img: 'http://www.yaelyspizza.com/wp-content/uploads/2017/06/buffets-familiares-en-cdmx-carne-pizza-y-videojuegos-02.jpg',
+  },
+  {
+    img: 'http://www.seriouseats.com/recipes/images/2017/02/20170216-detroit-style-pizza-43-1500x1125.jpg',
+  },
+  {
+    img: 'https://www.landolakes.com/RecipeManagementSystem/media/Recipe-Media-Files/Recipes/Retail/x17/18691-ice-cream-cookie-pizza-600x600.jpg?ext=.jpg',
+  },
+  {
+    img: 'https://www.pizzaparma.us/wp-content/uploads/2017/07/Pizza_Parma_Pittsburgh_Pizza_Dinner__Lunch_Caterin.jpg',
+  },
+  {
+    img: 'https://nickpassey.com/wp-content/uploads/2016/10/Featured.jpg',
+  },
+];
+
+const GridListsPage = ({ className }) => (
+  <MaterialThemeProvider theme={{ primary: '#03A9F4' }}>
+    <div className={className}>
+      <h1>Grid list</h1>
+      <h3>Row</h3>
+      <p>Gridlists default to flex-direction: row (and are responsive)</p>
+      <GridList>
+        {tilesData.map(tile => (
+          <GridTile
+            key={tile.img}
+          >
+            <Img src={tile.img} />
+          </GridTile>
+        ))}
+      </GridList>
+      <h3>Column Grid</h3>
+      <p>Change flex-direction to column by passing in "column"</p>
+      
+      <div height="200px">
+        <GridList column>
+          {tilesData.map(tile => (
+            <GridTile
+              key={tile.img}
+            >
+              <Img src={tile.img} />
+            </GridTile>
+          ))}
+        </GridList>
+      </div>
+
+    </div>
+  </MaterialThemeProvider>
+);
+
+const StyledGridListsPage = styled(GridListsPage)`
+  padding: 25px;
+  width: 100%;
+  height: 100%;
+`;
+
+export default StyledGridListsPage;
--- pages/index.js
+++ pages/index.js
@@ -3,40 +3,62 @@ import styled from 'styled-components';
 import Link from 'next/link';
 import MaterialThemeProvider from '../src/theme/ThemeProvider';
 
+import List from '../src/components/List/List';
+import ListItem from '../src/components/List/ListItem';
+
 const HomePage = ({ className }) => (
   <MaterialThemeProvider theme={{ primary: '#03A9F4' }}>
-    <div className={className}>
-      <h1>Placeholder</h1>
-      <Link href="/appbar"><a>Appbars Page</a></Link>
-      <br />
-      <Link href="/buttons"><a>Buttons Page</a></Link>
-      <br />
-      <Link href="/cards"><a>Cards Page</a></Link>
-      <br />
-      <Link href="/text-fields"><a>TextFields Page</a></Link>
-      <br />
-      <Link href="/tabs"><a>Tabs Page</a></Link>
-      <br />
-      <Link href="/dialog"><a>Dialog Page</a></Link>
-      <br />
-      <Link href="/lists"><a>Lists Page</a></Link>
-      <br />
-      <Link href="/sliders"><a>Sliders Page</a></Link>
-      <br />
-      <Link href="/tables"><a>Tables Page</a></Link>
-      <br />
-      <Link href="/snackbar"><a>Snackbar Page</a></Link>
-      <br />
-      <Link href="/drawer"><a>Drawers Page</a></Link>
-      <br />
-      <Link href="/divider"><a>Divider Page</a></Link>
-      <br />
-      <Link href="/chips"><a>Chip Page</a></Link>
-    </div>
+    <List className={className}>
+      <h1>Styled Material Components</h1>
+      <ListItem>
+        <Link href="/appbar"><a>Appbars Page</a></Link>
+      </ListItem>
+      <ListItem>
+        <Link href="/buttons"><a>Buttons Page</a></Link>
+      </ListItem>
+      <ListItem>
+        <Link href="/cards"><a>Cards Page</a></Link>
+      </ListItem>
+      <ListItem>
+        <Link href="/chips"><a>Chips Page</a></Link>
+      </ListItem>
+      <ListItem>
+        <Link href="/dialog"><a>Dialog Page</a></Link>
+      </ListItem>
+      <ListItem>
+        <Link href="/divider"><a>Divider Page</a></Link>
+      </ListItem>
+      <ListItem>
+        <Link href="/drawer"><a>Drawers Page</a></Link>
+      </ListItem>
+      <ListItem>
+        <Link href="/gridlists"><a>Grid Lists Page</a></Link>
+      </ListItem>
+      <ListItem>
+        <Link href="/lists"><a>Lists Page</a></Link>
+      </ListItem>
+      <ListItem>
+        <Link href="/tabs"><a>Tabs Page</a></Link>
+      </ListItem>
+      <ListItem>
+        <Link href="/tables"><a>Tables Page</a></Link>
+      </ListItem>
+      <ListItem>
+        <Link href="/text-fields"><a>TextFields Page</a></Link>
+      </ListItem>
+      <ListItem>
+        <Link href="/sliders"><a>Sliders Page</a></Link>
+      </ListItem>
+      <ListItem>
+        <Link href="/snackbar"><a>Snackbar Page</a></Link>
+      </ListItem>
+    </List>
   </MaterialThemeProvider>
 );
 
 const StyledHomePage = styled(HomePage)`
+  padding: 25px;
+
   & button {
     margin: 16px;
   }

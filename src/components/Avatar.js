5064f890403d55c98990523a80d7c288a5cdc106
bennett staley <me@bennettstaley.com>
2018-02-23 10:56:44 -0600

    fix(Avatar): Import React
    Fixes the react undefined issue when using the SMC module in a non
    next-build env

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,9 +1,10 @@
+import React from 'react';
 import { Circular } from './Circular';
 import { Image } from './Image';
 
 export const Avatar = Circular.extend.attrs({
   children: props => (props.src ? <Image src={props.src} /> : props.children),
-})`
+}) `
   // This is just here because we have to have some sort of styles applied at this level.
   text-align: left;
 `;

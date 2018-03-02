fa2df3f7c7653748fea22c2eeee0bf1559cec166
martahj <marta.hourigan.johnson@gmail.com>
2018-03-01 19:40:11 -0600

    refactor(Tabs): Export subcomponents
    Exports subcomponents so they can be styled

--- pages/tabs.js
+++ pages/tabs.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Tabs from '../src/components/Tabs/Tabs';
-import Tab from '../src/components/Tabs/Tab';
+import styled from 'styled-components';
+import { Tabs, Tab, TabsContainer, TabNavigationContainer } from '../src/index';
 import MaterialThemeProvider from '../src/theme/ThemeProvider';
 
 // TODO: animations (inkbar && ripple)
@@ -8,6 +8,17 @@ import MaterialThemeProvider from '../src/theme/ThemeProvider';
 // TODO: accept onChange as props
 // TODO: (future) scrollable tabs
 // TODO: controlled tabs
+
+// This won't work with Tabs.extend, so need to create a new styled-component
+const WithStyledTabContainer = styled.div`
+  ${TabsContainer} {
+    background-color: yellow;
+  }
+
+  ${TabNavigationContainer} {
+    background-color: orange;
+  }
+`
 
 const TabsPage = ({ className }) => (
   <MaterialThemeProvider theme={{ primary: '#03A9F4' }}>
@@ -60,6 +71,26 @@ const TabsPage = ({ className }) => (
           Totally tabular
         </Tab>
       </Tabs>
+      <h2>With subcomponents styled</h2>
+      <WithStyledTabContainer>
+        <Tabs
+          width='130'
+          activeBackgroundColor='#b5211e'
+          passiveBackgroundColor='#df4340'
+          activeFontColor='#fff'
+          passiveFontColor='#c2c2c2'
+          showInkbar={false}>
+          <Tab label='Tab One'>
+            First tab content is great
+          </Tab>
+          <Tab label='Tab Two'>
+            Tab two has content as well
+          </Tab>
+          <Tab label='Tab Three'>
+            Totally tabular
+          </Tab>
+        </Tabs>
+      </WithStyledTabContainer>
     </div>
   </MaterialThemeProvider>
 );

7839ff9efebb41e593ca6283dca9b199092d5288
ccm36 <cameron.c.malloy@gmail.com>
2018-03-19 15:57:53 -0500

    Sticky subheader to parent grid component

--- src/components/GridList/GridSubheader.js
+++ src/components/GridList/GridSubheader.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 const GridSubheaderComponent = ({ className, title }) => (
   <div className={`${className} smc-grid-subheader`}>
+    {console.log('gridList elements: ', React)}
     {title && (
       <div>
         {title}
@@ -11,30 +12,13 @@ const GridSubheaderComponent = ({ className, title }) => (
   </div>
 );
 
-// if header is on the page but not scrolled past
-  // position: relative
-// else (until next subheader reaches top)
-  // position: fixed;
-  // box-shadow: 0 5px rgba(0,0,0,0.1);
-
 const GridSubheader = styled(GridSubheaderComponent)`
-  font-family: "Roboto";
-  font-size: 14px;
-  font-weight: 500;
   text-align: left;
-  top: 0;
+  top: 0px;
   height: 48px;
   width: 100%;
   padding-left: 16px;
-
-  > .smc-grid-subheader {
-    position: relative;
-  }
-
-  > .smc-grid-subheader-fixed {
-    position: fixed;
-    box-shadow: 0 5px rgba(0,0,0,0.1);
-  }
+  position: sticky;
 `;
 
 export default GridSubheader;
--- src/components/GridList/index.js
+++ src/components/GridList/index.js
@@ -1,2 +1,3 @@
 export { default as GridList } from './GridList';
 export { default as GridTile } from './GridTile';
+export { default as GridSubheader } from './GridSubheader';

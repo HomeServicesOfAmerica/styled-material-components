b85ce3bace2013dd2fdddf9698eb6c012e0d2507
arilfrankel <ari.l.frankel@gmail.com>
2018-04-03 18:50:02 -0500

    feat(menu): open to space
    Open menu to where there is space for it. If there is not room for it below open vertically. If there is not room to the right open to the left.

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
+import debounce from 'lodash.debounce';
 import elevation from '../../mixins/elevation';
 import MenuList from './MenuList';
 import MenuItem from './MenuItem';
@@ -7,59 +8,79 @@ import MenuItem from './MenuItem';
 class MenuComponent extends Component {
   componentDidMount() {
     document.addEventListener('mousedown', this.handleClick, false);
+    document.addEventListener('scroll', this.recalculatePosition);
+    window.addEventListener('resize', this.recalculatePosition);
   }
 
   componentWillUnmount() {
     document.removeEventListener('mousedown', this.handleClick, false);
+    document.removeEventListener('scroll', this.recalculatePosition);
+    window.removeEventListener('resize', this.recalculatePosition);
   }
 
   handleClick = (event) => {
     if (this.menu.contains(event.target)) return;
     this.props.onClose && this.props.onClose(event);
-  }
+  };
+  
+  recalculatePosition = debounce(() => {
+    const { props: { anchorEl }, menu } = this;
+    if (!anchorEl || !menu) return;
+    const anchorRect = anchorEl.getBoundingClientRect();
+    const menuRect = this.menu.getBoundingClientRect();
+    const left = anchorEl.getBoundingClientRect().x;
+    const top = anchorEl.getBoundingClientRect().y;
+    const overBottom = top + menuRect.height > window.innerHeight;
+    const overRight = left + menuRect.width > window.innerWidth;
+    this.menu.style.top = `${anchorRect.top -
+      (overBottom ? menuRect.height - anchorRect.height : 0)}px`;
+    this.menu.style.left = `${anchorRect.left -
+      (overRight ? menuRect.width - anchorRect.width : 0)}px`;
+  }, 0);
 
   render() {
     const { className, children, menuItems, open, onClose } = this.props;
     const renderMenuItems = open && menuItems && menuItems.length > 0;
     const renderChildren = open && !menuItems;
-
     return (
-      <div className={`${className} smc-Menu`} ref={(ref) => {this.menu = ref;}}>
-        {renderChildren &&
+      <div
+        className={`${className} smc-Menu`}
+        ref={(ref) => {
+          this.menu = ref;
+          this.recalculatePosition();
+        }}
+      >
+        {renderChildren && (
           <MenuList onKeyDown={this.handleKeyDown} onClose={onClose}>
             {children}
           </MenuList>
-        }
-        {renderMenuItems &&
+        )}
+        {renderMenuItems && (
           <MenuList onKeyDown={this.handleKeyDown} onClose={onClose}>
-            {menuItems.map(item =>
-              <MenuItem key={item.text} onClick={item.onClick}>{item.text}</MenuItem>)}
+            {menuItems.map(item => (
+              <MenuItem key={item.text} onClick={item.onClick}>
+                {item.text}
+              </MenuItem>
+            ))}
           </MenuList>
-        }
+        )}
       </div>
     );
   }
 }
 
-const positionStyles = css`
-  top: ${props => props.anchorEl && props.anchorEl.getBoundingClientRect().top}px;
-  left: ${props => props.anchorEl && props.anchorEl.getBoundingClientRect().left}px;
-`;
-
 const Menu = styled(MenuComponent)`
   padding: 0;
   position: absolute;
-  ${positionStyles}
   box-sizing: border-box;
   border-radius: 2px;
   overflow: hidden;
-  display:inline-block;
+  display: inline-block;
   min-width: 72px;
   max-width: 280px;
   z-index: 5;
   background: #fff;
-  ${elevation(2)};
+  ${elevation(3)};
 `;
-
 
 export default Menu;

17d1d54455ac3b312a398699df032603c53c4df3
ceciliamvrie <ceciliamvrie@gmail.com>
2018-04-04 10:50:24 -0500

    refactor(Tooltip): Update styles
    Adds some small edits to the toolip styles

--- pages/tooltip.js
+++ pages/tooltip.js
@@ -1,31 +1,23 @@
 import styled from 'styled-components';
-import { ThemeProvider, Tooltip, TooltipLink, TooltipLabel } from '../src';
+import { ThemeProvider, Tooltip } from '../src';
 
 const SpacerToCheckScrollBehavior = styled.div`
   height: 400px;
-`
+`;
+
 const TooltipPageComponent = ({ className }) => (
   <ThemeProvider>
     <div className={className}>
       <h2>Tooltips</h2>
       <h6>See spec at https://material.io/guidelines/components/tooltips.html#</h6>
-      <p>
-        Pass in whatever content you would like as a child of the tooltip component.
-        *Teeechnically* material design wants you to only pass in a string, but you
-        can pass in a react component as well. Images are not supported yet because
-        on initial render they aren't calculated into the portal height.
-      </p>
+      <p>{"Pass in whatever content you would like as a child of the tooltip component. *Teeechnically* material design wants you to only pass in a string, but you can pass in a react component as well. Images are not supported yet because on initial render they aren't calculated into the portal height."}</p>
       Takes the following props:
       <ul>
         <li>
           mobile?: boolean. Indicates whether or not the tooltip uses the mobile
           designs.
         </li>
-        <li>
-          width?: number. Optional but recommended. If you do not pass this in
-          and you pass in a string as the tooltip content, it will try to guess
-          the width, but it's not very good at it. If you pass in a react component,
-          it will default to 30px, which is unlikley to be what you want.
+        <li>{"width?: number. Optional but recommended. If you do not pass this in and you pass in a string as the tooltip content, it will try to guess the width, but it's not very good at it. If you pass in a react component it will default to 30px, which is unlikley to be what you want."}
         </li>
         <li>
           link?: React.Node | string. This will default to the info icon, but you
@@ -40,13 +32,13 @@ const TooltipPageComponent = ({ className }) => (
       <h3>Desktop Tooltip with React component as child</h3>
       <Tooltip
         contentWidth={200}
-        >
-          <div>
-            You can also pass in a react component!
-            <p>
-              But be sure to pass in a width prop to the tooltip or it will probably look terrible
-            </p>
-          </div>
+      >
+        <div>
+          You can also pass in a react component!
+          <p>
+            But be sure to pass in a width prop to the tooltip or it will probably look terrible
+          </p>
+        </div>
       </Tooltip>
       <h3>Desktop tooltip with custom link</h3>
       <Tooltip link={() => <span>custom link</span>}>
@@ -71,7 +63,7 @@ const TooltipPageComponent = ({ className }) => (
       <SpacerToCheckScrollBehavior />
     </div>
   </ThemeProvider>
-)
+);
 
 export default styled(TooltipPageComponent)`
   margin-left: 20%;
